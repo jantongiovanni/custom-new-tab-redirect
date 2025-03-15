317cd4a92437d9a5ae8ce4d88ff3fe695113ff8e
Joe A <janton153@gmail.com>
2025-03-15 08:02:43 -0700

    set color mode

--- src/options/options.js
+++ src/options/options.js
@@ -1,35 +1,76 @@
 console.log('options.ts');
 
 //set the default value of the url and color inputs
-chrome.storage.sync.get({ url: '', color: '' }, items => {
+chrome.storage.sync.get({ url: '', mode: 'dark', color: '' }, items => {
+  console.log(items);
+
   const urlElement = document.getElementById('url');
-  const schemeElement = document.getElementById('scheme');
+  const modeDarkElement = document.getElementById('mode-dark');
+  const modeLightElement = document.getElementById('mode-light');
+  const modeCustomElement = document.getElementById('mode-custom');
   const colorElement = document.getElementById('color');
+  const colorPicker = document.getElementById('color-picker');
 
   if (urlElement) {
     urlElement.value = items?.url || '';
   }
-  if (schemeElement) {
-    schemeElement.value = items?.scheme || 'dark';
+  if (modeDarkElement && modeLightElement && modeCustomElement) {
+    if (items?.mode === 'dark') {
+      modeDarkElement.checked = true;
+      colorPicker.style.display = 'none';
+    } else if (items?.mode === 'light') {
+      modeLightElement.checked = true;
+      colorPicker.style.display = 'none';
+    } else if (items?.mode === 'custom') {
+      modeCustomElement.checked = true;
+      colorPicker.style.display = 'block';
+    }
   }
   if (colorElement) {
-    colorElement.value = items?.color || '#000000';
+    colorElement.value = items?.color || '#121212';
   }
 });
 
+document.querySelectorAll('input[name="mode"]').forEach(element => {
+  element.addEventListener('change', function() {
+    const colorPicker = document.getElementById('color-picker');
+    switch (this.value) {
+      case 'custom':
+      colorPicker.style.display = 'block';
+      break;
+      case 'light':
+      colorPicker.style.display = 'none';
+      document.getElementById('color').value = '#FFFFFF';
+      break;
+      case 'dark':
+      colorPicker.style.display = 'none';
+      document.getElementById('color').value = '#121212';
+      break;
+      default:
+      colorPicker.style.display = 'none';
+    }
+  });
+});
 
 document.getElementById("save").addEventListener("click", function() {
-    console.log('save');
-  
-  const color = document.getElementById('color').value;
-  chrome.storage.sync.set({ color });
-  const url = document.getElementById('url').value;
+  console.log('save');
+
+  const color = document.getElementById('color')?.value;
+  color && chrome.storage.sync.set({ color });
+  const url = document.getElementById('url')?.value || '';
   chrome.storage.sync.set({ url });
+  const mode = document.querySelector('input[name="mode"]:checked')?.value;
+  mode && chrome.storage.sync.set({ mode });
   const status = document.getElementById('status');
   status.textContent = '✅ Settings saved.';
   setTimeout(() => {
     status.textContent = '';
   }, 2000);
+
+  //console log current storage settings
+  chrome.storage.sync.get({ url: '', mode: 'dark', color: '' }, items => {
+    console.log(items);
+  });
 });
 
 
@@ -37,12 +78,6 @@ document.getElementById("save").addEventListener("click", function() {
 document.getElementById('reset-url').addEventListener('click', function() {
   console.log('reset');
   document.getElementById('url').value = '';
-  chrome.storage.sync.remove('url');
-  const status = document.getElementById('status');
-  status.textContent = 'URL reset.';
-  setTimeout(() => {
-    status.textContent = '';
-  }, 2000);
 });
 
 document.getElementById('open-extension-settings').addEventListener('click', function() {
